d59c835e4cc758ff8d6ba5ae5729e4f0ee6e8783
Albert Arques <albertarques@gmail.com>
2022-09-16 11:26:09 +0200

    Commented firebase.js out of use

--- firebase.js
+++ firebase.js
@@ -1,27 +1,27 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
 
-// Import the functions you need from the SDKs you need
+// // Import the functions you need from the SDKs you need
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyDZAqX_MlxYLf4CWSc0FoeyaF6Pwx_n3HY",
+// // Initialize Firebase
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDZAqX_MlxYLf4CWSc0FoeyaF6Pwx_n3HY",
 
-  authDomain: "tesla-webclone-73930.firebaseapp.com",
+//   authDomain: "tesla-webclone-73930.firebaseapp.com",
 
-  projectId: "tesla-webclone-73930",
+//   projectId: "tesla-webclone-73930",
 
-  storageBucket: "tesla-webclone-73930.appspot.com",
+//   storageBucket: "tesla-webclone-73930.appspot.com",
 
-  messagingSenderId: "847032652667",
+//   messagingSenderId: "847032652667",
 
-  appId: "1:847032652667:web:ca044220c8b06d183823de",
-};
+//   appId: "1:847032652667:web:ca044220c8b06d183823de",
+// };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
 
-export { auth, app };
+// export { auth, app };
