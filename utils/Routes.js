f1dbb1a0529cf760ac46e95ad351a177afe0fb61
Albert Arques <albertarques@gmail.com>
2022-09-18 16:19:49 +0200

    Routes component arranged

--- utils/Routes.js
+++ utils/Routes.js
@@ -1,16 +1,13 @@
-import { useRouter } from "next/router";
+import { push } from "next/router";
 
 export function GoToHome() {
-  const { push } = useRouter();
   push("/");
 }
 
 export function GoToRegistered() {
-  const { push } = useRouter();
   push("/registered");
 }
 
 export function GoToLoggedIn() {
-  const { push } = useRouter();
   push("/logged");
 }
