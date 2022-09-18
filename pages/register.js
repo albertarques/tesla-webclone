66127cc7d3dd78a997456ff94aac14fbc761ab0f
Albert Arques <albertarques@gmail.com>
2022-09-18 09:11:03 +0200

    Register component operational with 1 warning

--- pages/register.js
+++ pages/register.js
@@ -2,80 +2,67 @@ import Layout from "../components/Layout";
 import Form from "../components/Form";
 import Input from "../components/bites/Input";
 import Button from "../components/bites/Button";
-import { auth } from "../firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { useRouter } from "next/router";
 import { useState } from "react";
 import HeroBlock from "../components/HeroBlock";
+import { registerUser } from "../lib/Firebase";
+import { useRouter } from "next/router";
+import { GoToRegistered } from "../utils/Routes";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../lib/Firebase";
 
 export default function Register() {
-  const [Username, setUsername] = useState();
-  const [Password, setPassword] = useState();
-  const [ValPassword, setValPassword] = useState();
+  // On every page that need user, loading or error, we need to refer useStateAuth
+  const [user] = useAuthState(auth);
 
-  const { push } = useRouter();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [valPassword, setValPassword] = useState();
 
-  // Font: https://github.com/mukulrajpoot262610/dev-case-app
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    // Validation email regex
-    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(Username)) {
-      return alert("Has introducido una dirección de correo no válida.");
-    }
-    // Validation password -> valPassword
-    if (Password !== ValPassword)
-      return alert("Las contraseñas deben coincidir.");
-
-    try {
-      await createUserWithEmailAndPassword(auth, Username, Password).then(
-        (userCredential) => {
-          const user = userCredential.user;
-          console.log(user);
-        }
-      );
-      push("/registered");
-    } catch (error) {
-      console.log(error.code); // 'auth/network-request-failed'
-      console.log(error.message); // 'Firebase: Error (auth/network-request-failed).'
-      console.log(error.name); // 'FirebaseError'
-      console.log(error.customData); // {}
-    }
-  };
-
-  return (
-    <Layout>
-      <HeroBlock form picture="/img/hero@2.jpg" alt="Modelo S en color rojo">
-        <Form submit={handleSubmit} title="Registro">
-          <Input
-            id="user"
-            name="email"
-            type="email"
-            label="Introduzca su email"
-            minLength="10"
-            maxLength="20"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <Input
-            id="password"
-            name="password"
-            type="password"
-            label="Introduzca su contraseña"
-            minLength="10"
-            maxLength="20"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Input
-            id="valPassword"
-            name="valPassword"
-            type="password"
-            label="Repita su contraseña"
-            minLength="10"
-            maxLength="20"
-            onChange={(e) => setValPassword(e.target.value)}
-          />
-          <Button text="Enviar" />
-        </Form>
-      </HeroBlock>
-    </Layout>
-  );
+  if (user) {
+    GoToRegistered();
+  }
+  if (!user) {
+    return (
+      <Layout>
+        <HeroBlock form picture="/img/hero@2.jpg" alt="Modelo S en color rojo">
+          <Form
+            submit={(e) => registerUser(e, email, password, valPassword)}
+            title="Registro"
+          >
+            <Input
+              id="user"
+              name="email"
+              type="email"
+              label="Introduzca su email"
+              minLength="10"
+              maxLength="20"
+              autocomplete="off"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              label="Introduzca su contraseña"
+              minLength="10"
+              maxLength="20"
+              autocomplete="off"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Input
+              id="valPassword"
+              name="valPassword"
+              type="password"
+              label="Repita su contraseña"
+              minLength="10"
+              maxLength="20"
+              autocomplete="off"
+              onChange={(e) => setValPassword(e.target.value)}
+            />
+            <Button text="Enviar" />
+          </Form>
+        </HeroBlock>
+      </Layout>
+    );
+  }
 }
