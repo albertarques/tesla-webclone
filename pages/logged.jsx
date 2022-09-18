a7de81ba3d605e0a4723a7f2a449e6affd4b1407
Albert Arques <albertarques@gmail.com>
2022-09-18 09:10:23 +0200

    Logged component behavior

--- pages/logged.jsx
+++ pages/logged.jsx
@@ -1,21 +1,26 @@
-import { useRouter } from "next/router";
 import HeroBlock from "../components/HeroBlock"
 import Layout from "../components/Layout"
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../lib/Firebase";
+import { GoToHome } from "../utils/Routes";
 
 export default function Logged() {
 
-  const {push} = useRouter();
+  const [user] = useAuthState(auth);
 
-  const result = 
-    <Layout>
-      <HeroBlock
-        picture="/img/25-Hero-D.jpg"
-        alt="Casa con paneles solares en el tejado"
-        title={"Hola..."+user.email}
-      >
-      </HeroBlock>
-    </Layout>
-
-    return result;    
+  if(user){
+    return (
+      <Layout>
+        <HeroBlock
+          picture="/img/25-Hero-D.jpg"
+          alt="Casa con paneles solares en el tejado"
+          title={"Hola "+user.email}
+        >
+        </HeroBlock>
+      </Layout>  
+    )  
+  } else {
+      {GoToHome()}
+  }
 }
 
