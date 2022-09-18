38894aec5c4edd6598f55a7fc8fb376d0d6cf250
Albert Arques <albertarques@gmail.com>
2022-09-18 15:39:50 +0200

    Updating method post on login component

--- components/Form.jsx
+++ components/Form.jsx
@@ -3,7 +3,7 @@ import styles from "../styles/Form.module.css"
 
 export default function Form({children, title, submit}) {
   const result =
-    <form className={styles.form} onSubmit={submit}>
+    <form className={styles.form} onSubmit={submit} method="post">
       <h2 className={styles.h2}>{title}</h2>
       {children}
     </form>
