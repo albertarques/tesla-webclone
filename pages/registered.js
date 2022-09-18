a6de500ad2bb5efa56dd29d477d30cc6ff582169
Albert Arques <albertarques@gmail.com>
2022-09-18 16:00:56 +0200

    Change jsx to js extension

---
+++ pages/registered.js
@@ -0,0 +1,22 @@
+import HeroBlock from "../components/HeroBlock";
+import Layout from "../components/Layout";
+import { GoToHome } from "../utils/Routes";
+
+export default function Registered() {
+  const [user] = useAuthState(auth);
+
+  if (!user) {
+    return (
+      <Layout>
+        <HeroBlock
+          picture="/img/25-Hero-D.jpg"
+          alt="Casa con paneles solares en el tejado"
+          title="Ha sido registrado con éxito!"
+          p="Proximamente recibirá todas nuestras noticias."
+        ></HeroBlock>
+      </Layout>
+    );
+  } else {
+    GoToHome();
+  }
+}
